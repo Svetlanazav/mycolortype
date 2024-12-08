a1f72c130e0f9839abf568db816f4eebc5ffa590
Svetazav <project.manager@it-lead.eu>
2024-12-08 21:10:03 +0100

    1

--- src/scripts/segmentation.ts
+++ src/scripts/segmentation.ts
@@ -12,10 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {
-  ImageSegmenter,
-  FilesetResolver,
-} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2";
+import { ImageSegmenter, FilesetResolver } from "@mediapipe/tasks-vision";
 
 // Get DOM elements
 const video = document.getElementById("webcam") as HTMLVideoElement;
